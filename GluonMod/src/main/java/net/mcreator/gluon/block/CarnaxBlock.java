0c233746151010e304dbbbd0c20b96e0800ef84b
CerisaCo <19498107+cdwc25@users.noreply.github.com>
2020-07-12 19:34:20 -0400

    Merged GluonMod

---
+++ GluonMod/src/main/java/net/mcreator/gluon/block/CarnaxBlock.java
@@ -0,0 +1,65 @@
+
+package net.mcreator.gluon.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.IWorldReader;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.gluon.GluonElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@GluonElements.ModElement.Tag
+public class CarnaxBlock extends GluonElements.ModElement {
+	@ObjectHolder("gluon:carnax")
+	public static final Block block = null;
+	public CarnaxBlock(GluonElements instance) {
+		super(instance, 2);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(80f, 1200f).lightValue(0).harvestLevel(3)
+					.harvestTool(ToolType.PICKAXE));
+			setRegistryName("carnax");
+		}
+
+		@Override
+		public boolean isBeaconBase(BlockState state, IWorldReader world, BlockPos pos, BlockPos beacon) {
+			return true;
+		}
+
+		@Override
+		public MaterialColor getMaterialColor(BlockState state, IBlockReader blockAccess, BlockPos pos) {
+			return MaterialColor.LIME;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
